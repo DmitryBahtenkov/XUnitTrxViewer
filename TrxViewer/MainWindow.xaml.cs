1771cc5c67cd7845817cd3eed628901d7ec33c17
DmitryBahtenkov <cool.iridiy12soul@gmail.com>
2021-12-16 23:11:35 +0300

    first commit

---
+++ TrxViewer/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace TrxViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string Path = @"C:\Users\Andrew\Documents\Workspace\Sharp\TrxViewer\ДмитрийБахтенков_DESKTOP_F6SKA47_2021_12_16_16_16_28.trx";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetTests();
+        }
+
+        public string ReadFile()
+        {
+            return File.ReadAllText(Path);
+        }
+
+        public void GetTests()
+        {
+            var xml = ReadFile();
+            xml = new string(xml.SkipWhile(x => x != '\n').ToArray());
+
+            var regex = new Regex("xmlns=\".*\"");
+
+            xml = regex.Replace(xml, "");
+            XmlSerializer serializer = new XmlSerializer(typeof(TestRun));
+            using (StringReader reader = new StringReader(xml))
+            {
+                var test = (TestRun)serializer.Deserialize(reader);
+                var testNameRegexp = new Regex(@"\(.*\)");
+                var tests = test.Results.First().UnitTestResult.Select(x=>
+                {
+                    x.TestName = x.TestName.Replace("Skillaz.Tests.", "");
+                    x.TestName = testNameRegexp.Replace(x.TestName, "").Split(".").Last();
+
+                    return x;
+                }).ToList();
+                ListTests.ItemsSource = tests;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ TrxViewer/TestRun.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace TrxViewer
+{
+    [XmlRoot(ElementName = "TestRun")]
+    public class TestRun
+    {
+        [XmlElement(ElementName = "Results")]
+        public List<Results> Results { get; set; }
+    }
+
+    [XmlRoot(ElementName = "UnitTestResult")]
+    public class UnitTestResult
+    {
+
+        [XmlAttribute(AttributeName = "executionId")]
+        public string ExecutionId { get; set; }
+
+        [XmlAttribute(AttributeName = "testId")]
+        public string TestId { get; set; }
+
+        [XmlAttribute(AttributeName = "testName")]
+        public string TestName { get; set; }
+
+        [XmlAttribute(AttributeName = "computerName")]
+        public string ComputerName { get; set; }
+
+        [XmlAttribute(AttributeName = "duration")]
+        public DateTime Duration { get; set; }
+
+        [XmlAttribute(AttributeName = "startTime")]
+        public DateTime StartTime { get; set; }
+
+        [XmlAttribute(AttributeName = "endTime")]
+        public DateTime EndTime { get; set; }
+
+        [XmlAttribute(AttributeName = "testType")]
+        public string TestType { get; set; }
+
+        [XmlAttribute(AttributeName = "outcome")]
+        public string Outcome { get; set; }
+
+        [XmlAttribute(AttributeName = "testListId")]
+        public string TestListId { get; set; }
+
+        [XmlAttribute(AttributeName = "relativeResultsDirectory")]
+        public string RelativeResultsDirectory { get; set; }
+
+        [XmlElement(ElementName = "Output")]
+        public Output Output { get; set; }
+
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Output")]
+    public class Output
+    {
+
+        [XmlElement(ElementName = "StdOut")]
+        public string StdOut { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Results")]
+    public class Results
+    {
+
+        [XmlElement(ElementName = "UnitTestResult")]
+        public List<UnitTestResult> UnitTestResult { get; set; }
+    }
+}
